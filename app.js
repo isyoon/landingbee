90af138f652e886b2cb8daa24f6ebb2c964cfa38
Yoon <isyoonn@gmail.com>
2013-03-14 02:29:39 -0700

    doing ~

--- app.js
+++ app.js
@@ -4,14 +4,15 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mysql = require('mysql')
+  , connection = mysql.createConnection(process.env.DATABASE_URL|| 'mysql://be12fd3dd3f884:d6377262@us-cdbr-east-03.cleardb.com/heroku_2a584efb2f39340?reconnect=true');
 
-var app = express();
+app = express();
 
 app.configure(function(){
+  app.db = connection;
   app.set('port', process.env.PORT || 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -22,16 +23,40 @@ app.configure(function(){
   app.use(express.cookieParser('__landingbee__'));
   app.use(express.session());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+  //app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
-});
+});  
 
+var routes = require('./routes')
+  , handleDisconnect = function(connection) {
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(connection.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+};
+
+handleDisconnect(connection);
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/admin/:password?', routes.admin);
+app.get('/nimda/category/add', routes.addCategory);
+app.post('/nimda/category/save', routes.saveCategory);
+app.get('/nimda/store/add', routes.addStore);
+app.get('/nimda/store/stats', routes.showStats);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,39 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Landingbee' });
+var reqs = exports,
+  db = app.db;
+
+reqs.index = function(req, res) {
+    res.render('index', { title: 'Landingbee' });
+};
+
+reqs.admin = function(req, res) {
+  if(req.params.password !== 'IamAdmin!!') {
+    res.redirect('/');
+    return;
+  }
+  res.render('admin', {title: 'admin'});
+};
+
+reqs.addCategory = function(req, res) {
+  res.render('add-category', {title: 'admin - add category'});
+};
+
+reqs.saveCategory = function(req, res) {
+  var data = {name: req.body.name, order: 1};
+  console.log(data);
+  var query = db.query('INSERT INTO category SET ?', data, function(err, result) {
+    console.log(query);
+    console.log(result);
+    res.redirect('/nimda/category/add');
+  });
+};
+
+reqs.addStore = function(req, res) {
+  res.render('add-store', {title: 'admin - add store'});
+};
+
+reqs.showStats = function(req, res) {
+  res.render('show-stats', {title: 'admin - show stats'});
 };
